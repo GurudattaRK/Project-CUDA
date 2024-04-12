1b2c43e9a658c0d9080aed8bb3fc0207aa26f997
Gurudatta R K <86517260+GurudattaRK@users.noreply.github.com>
2024-04-12 17:06:29 +0530

    Kernel Update

--- kernel.cu
+++ kernel.cu
@@ -12,17 +12,16 @@ __global__ void supCUDA(char* key,char* initaddr,int itersize, int rounds )
     
     unsigned long long int  hostaddr =(unsigned long long int)initaddr ;
 
-    if(threadIdx.x == 0)
-    printf("%d iter = %d : %llu\n",id,itersize,hostaddr);
-
     hostaddr = hostaddr + (id*128);
     // unsigned long long int addr = hostaddr + (id*itersize) ;
     char* thread_addr = (char*)hostaddr;
-    unsigned char tic=32;
+
+    if(threadIdx.x == 0)
+        printf("tid = %d : %llu\n",id,(unsigned long long int*)thread_addr);
+
     while (rounds >0)
     {
-        if(threadIdx.x == 0)
-        printf("tid = %d : %llu\n",id,(unsigned long long int*)thread_addr);
+        
         for(int i=0 ; i<128 ; i++)
         {
             thread_addr[i] = thread_addr[i]^key[i];
@@ -37,7 +36,6 @@ __global__ void supCUDA(char* key,char* initaddr,int itersize, int rounds )
 
 int main()
 {
-    // remove("1.txt");
     FILE* reader = fopen("1.txt","rb");
     FILE* writer = fopen("2.txt","wb");
 
@@ -48,10 +46,8 @@ int main()
     unsigned long long int total_size = blocks*warp_size*CIPHER_BLOCK*rounds;
 
     char* data = (char*)calloc(1,total_size);
-    // memset(data,69,total_size);
     char key[130] = "qwertyuiopasdfghjklzxcvbnm,ouoq,re,xqe0,qwertyuiopasdfghjklzxcvbnm,ouoq,re,xqe0,qwertyuiopasdfghjklzxcvbnm,ouoq,re,xqe0,wuriov\0\0";
 
-    // Allocate memory on the device
     char *CudaData, *CudaKey;
 
     
@@ -61,7 +57,7 @@ int main()
 
     printf("1\n");
     cudaMalloc((void**)&CudaData, total_size);
-    // cudaMalloc((void**)&deviceOutput, BUF_SIZE);
+
     cudaMalloc((void**)&CudaKey, 129);
     printf("2\n");
 
@@ -77,7 +73,7 @@ int main()
 
 
     cudaMemcpy(data, CudaData,  total_size, cudaMemcpyDeviceToHost);
-    printf("%32767s\n",data);
+    // printf("%32767s\n",data);
     printf("6\n");
 
     cudaFree(CudaData);
@@ -85,15 +81,12 @@ int main()
 
 
     fwrite(data, total_size,1,writer);
-    // fwrite(data,1,32768,writer);
 
-    
     free(data);
 
     fclose(writer);
     fclose(reader);
     printf("7\n");
-
 
     return 0;
 }
