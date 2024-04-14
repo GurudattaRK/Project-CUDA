d0aca192d832087d91e1276e57033db5b6046399
Gurudatta R K <86517260+GurudattaRK@users.noreply.github.com>
2024-04-15 01:36:05 +0530

    Cuda Kernel Integrated

--- backend_Kernel.cu
+++ backend_Kernel.cu
@@ -45,10 +45,36 @@ void sequentiel(char* key,char* data,int rounds)
 
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 
-    // char* argt =  argv[1];
+    char* argt =  argv[1];
+    printf("\nArguement 1:%s\n",argt);
+    FILE* tempfile = fopen(argt,"r");
+    char line[257];
+    char *lines[5]; // Array to store pointers to each line string
+    int line_count = 0;
+
+    // Read and store lines
+    while (fgets(line, 257, tempfile) != NULL && line_count < 5) 
+    {
+        // Remove the newline character at the end of the line
+        line[strcspn(line, "\n")] = '\0';
+
+        // Skip empty lines
+        if (strlen(line) == 0) {
+            continue;
+        }
+
+        // Allocate memory for the line string and copy the line
+        lines[line_count] = (char*)malloc((strlen(line) + 1) * sizeof(char));
+        strcpy(lines[line_count], line);
+        line_count++;
+    }
+
+    fclose(tempfile);
+
+    //  char* argt =  argv[1];
     // char* argx = argv[2];
     // printf("\nArguement 1:%s\n",argt);
     // printf("Arguement 2:%s\n",argx);
@@ -60,20 +86,31 @@ int main()
     // int y= 3;//atoi(argx);
 
     // cudaMemGetInfo(&free_bytes, &total_bytes);
+
     unsigned long long int temp,residue,cuda_malloc_size,filesize,iter_size,max_free,available_mem,residue_offset,kernel_rounds=0;
     long long int rounds=0;
-    char *CudaData, *CudaKey;
+    char *CudaData, *CudaKey ,*inputfile, *outputfile,*size,*mode,*random;
 
+    inputfile = lines[1];
+    outputfile = lines[2];
+    size = lines[3];
+    mode = lines[4];
 
-
-    FILE *reader = fopen("1.txt","rb");
-    FILE* writer = fopen("2.txt","wb");
+    FILE *reader = fopen(inputfile,"rb");
+    FILE* writer = fopen(outputfile,"wb");
 
     int blocks =4,threads=32;
 
-    filesize = 128*128 + 128;
+    filesize  = strtoull(size,&random,10);
+    
     available_mem = 128*128;
-    char key[130] = "qwertyuiopasdfghjklzxcvbnm,ouoq,re,xqe0,qwertyuiopasdfghjklzxcvbnm,ouoq,re,xqe0,qwertyuiopasdfghjklzxcvbnm,ouoq,re,xqe0,wuriov\0\0";
+    char *key = lines[0] ;
+
+    printf("inputfile :%s\n",inputfile);
+    printf("outputfile :%s\n",outputfile);
+    printf("key :%s\n",key);
+    printf("size :%llu\n",size);
+    printf("mode :%s\n",mode);
 
 
 
@@ -261,6 +298,12 @@ int main()
 
     fclose(writer);
     fclose(reader);
+
+    for (int i = 0; i < line_count; i++) 
+    {
+        printf("Line %d: %s\n", i, lines[i]);
+        free(lines[i]);
+    }
 
     free(hostptr);
     // // printf("\n%s",deviceProp.name);
